a3a885f15945beeccf557750a004c6c7e2ba8163
talalalrashdi <dr.usb3@gmail.com>
2025-06-04 18:02:11 +0200

    Qrcode

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  # skip_before_action :authenticate_user!, only: [ :home ]
+
+
+  def welcome
+    
+    @generated_uuid = SecureRandom.uuid
+
+  end
+
 
   def home
+
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
+require "rqrcode"
+
 class SessionsController < ApplicationController
+
+  def index
+
+
+  end
+
+  def create
+
+   @session = Session.create
+   @session.user = current_user
+    @session.save
+   redirect_to session_by_uuid_path(@session.uuid)
+  end
+  def show
+
+
+
+    @session = Session.find_by!(uuid: params[:uuid])
+
+    # @session
+
+  end
+
 end
