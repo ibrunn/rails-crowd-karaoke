fa6ee02b971a9c548a784677fee41ae9acb075a9
ChiaJung Chung <heavenly0430@gmail.com>
2025-06-05 10:36:30 +0200

    setup in routes,htmls and css...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+  def genre_start
+  end
+
+  def genre_result
+  end
+
+  def genre_votes
+  end
+
+  def song_start
+  end
+
+  def song_result
+  end
+
+  def sing_start
+  end
+
+  def sing_end
+  end
+
+  def green_room
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Host
+  get "/sessions/:uuid/genre_start", to: "sessions#genre_start"
+  get "/sessions/:uuid/genres_result", to: "sessions#genre_result"
+  get "/sessions/:uuid/song_start", to: "sessions#song_start"
+  get "/sessions/:uuid/song_result", to: "sessions#song_result"
+  get "/sessions/:uuid/sing_start", to: "sessions#sing_start"
+  get "/sessions/:uuid/sing_end", to: "sessions#sing_end"
+  get "/sessions/:uuid/green_room", to: "sessions#green_room"
+
+  post "/sessions/:uuid/genre_votes", to: "sessions#create"
+
+  # Guests
 end
