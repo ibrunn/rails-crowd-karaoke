540f5558419ac3d3efac2a0c338740a5941428a3
Ingo von Brunn <repo@public-files.de>
2025-06-06 16:01:21 +0200

    changed qrcode to white

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,18 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
+  # current_guest_for_session method
+  # - Returns a cached result if already looked up
+  # - Returns nil if no guest_id in session
+  # - Looks up the Guest record by the ID stored in the session
+  # - Makes the method available to views with helper_method
+  def current_guest_for_session
+    return @current_guest if defined?(@current_guest)
+    return nil unless session[:guest_id].present?
+
+    @current_guest = Guest.find_by(id: session[:guest_id])
+  end
+  
+  helper_method :current_guest_for_session
+
 end
