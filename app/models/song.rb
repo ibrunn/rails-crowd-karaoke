3253ef6d5e6d7bd5de86368284b24e5b3a34cfd4
ChiaJung Chung <heavenly0430@gmail.com>
2025-06-10 17:33:04 +0200

    the background for the selected genre

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :genre
-  has_many :session_songs
-  has_many :song_votes, through: :session_songs, dependent: :destroy
+  has_many :game_session_songs
+  has_many :song_votes, through: :game_session_songs, dependent: :destroy
 
   validates :title, presence: true
   validates :artist, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_07_211009) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_10_144114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_07_211009) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   create_table "guests", force: :cascade do |t|
